f76ef23c66e659ca3006d9ad67041da0eb6a039f
Matteo Colombo <matte.colo@gmail.com>
2021-02-14 16:14:46 +0100

    clean live page

--- pages/live.vue
+++ pages/live.vue
@@ -1,13 +1,14 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="10" xl="8">
-      <iframe
-        src="https://player.twitch.tv/?channel=gmhikaru&parent=ioodemo.herokuapp.com&muted=true&autoplay=true"
-        height="600"
-        width="100%"
-        allowfullscreen="true"
-      >
-      </iframe>
+      <div class="container">
+        <iframe
+          src="https://player.twitch.tv/?channel=gmhikaru&parent=ioodemo.herokuapp.com&muted=true&autoplay=true"
+          class="responsive-iframe"
+          allowfullscreen="true"
+        >
+        </iframe>
+      </div>
       <template>
         <h1 href="#schedule" id="schedule" class="text-center">
           {{ title }}
@@ -16,29 +17,17 @@
       </template>
 
       <v-row justify="center" align="start">
-        <v-col cols="12" md="6" align="center">
-          <h2 align="center">{{ day1 }}</h2>
+        <v-col
+          cols="12"
+          md="6"
+          align="center"
+          v-for="day in schedule"
+          :key="day.day"
+        >
+          <h2 align="center">{{ day.day }}</h2>
           <v-timeline dense>
             <v-timeline-item
-              v-for="item in saturday"
-              :key="item.hour"
-              fill-dot
-              v-bind="item"
-            >
-              <v-card class="elevation-2">
-                <v-card-title align="justify">
-                  {{ item.activity }}
-                </v-card-title>
-                <v-card-subtitle align="start">{{ item.hour }}</v-card-subtitle>
-              </v-card>
-            </v-timeline-item>
-          </v-timeline>
-        </v-col>
-        <v-col cols="12" md="6" align="center">
-          <h2 align="center">{{ day1 }}</h2>
-          <v-timeline dense>
-            <v-timeline-item
-              v-for="item in sunday"
+              v-for="item in day.schedule"
               :key="item.hour"
               fill-dot
               v-bind="item"
@@ -66,105 +55,29 @@ export default {
   },
   data() {
     return {
+      channel: this.$t("live.channel"),
       title: this.$t("live.title"),
       desc: this.$t("live.desc"),
-      day1: this.$t("live.day1"),
-      day2: this.$t("live.day2"),
-      saturday: [
-        {
-          hour: "14:00",
-          activity: "Discord",
-          icon: "mdi-discord",
-          color: "#8A9CFE",
-        },
-        {
-          hour: "15:00",
-          activity: "Attività 1",
-          icon: "mdi-microphone",
-          color: "red",
-        },
-        {
-          hour: "16:00",
-          activity: "Attività 2",
-          icon: "mdi-puzzle",
-          color: "orange",
-        },
-        {
-          hour: "17:00",
-          activity: "Discord",
-          icon: "mdi-discord",
-          color: "#8A9CFE",
-        },
-        {
-          hour: "18:00",
-          activity: "Chi vuol essere milionario?",
-          icon: "mdi-chess-king",
-          color: "primary",
-        },
-        {
-          hour: "20:00",
-          activity: "Cena",
-          icon: "mdi-silverware-fork-knife",
-          color: "brown",
-        },
-        {
-          hour: "21:00",
-          activity: "Qualcosa insieme?",
-          icon: "mdi-brain",
-          color: "blue",
-        },
-      ],
-      sunday: [
-        {
-          hour: "15:00",
-          activity: "Finale Clock",
-          icon: "event-222",
-          color: "primary",
-        },
-        {
-          hour: "15:30",
-          activity: "Finale 4x4x4",
-          icon: "",
-          color: "secondary",
-        },
-        {
-          hour: "16:00",
-          activity: "Finale 3BLD",
-          icon: "",
-          color: "red",
-        },
-        {
-          hour: "16:30",
-          activity: "Finale Pyraminx",
-          icon: "",
-          color: "purple",
-        },
-        {
-          hour: "17:00",
-          activity: "Finale 2x2x2",
-          icon: "",
-          color: "red",
-        },
-        {
-          hour: "17:30",
-          activity: "Finale Square-1",
-          icon: "",
-          color: "secondary",
-        },
-        {
-          hour: "18:00",
-          activity: "Finale 3x3x3",
-          icon: "",
-          color: "primary",
-        },
-        {
-          hour: "19:00",
-          activity: "Premiazioni",
-          icon: "mdi-trophy",
-          color: "amber",
-        },
-      ],
+      schedule: this.$t("live.days"),
     };
   },
 };
 </script>
+
+<style scoped>
+.container {
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  padding-top: 56.25%; /* 16:9 Aspect Ratio (divide 9 by 16 = 0.5625) */
+}
+.responsive-iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
