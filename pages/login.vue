e3b4c645a766c7b18a872e819f2984d4f18b8288
Matteo Colombo <matte.colo@gmail.com>
2021-02-21 14:35:07 +0100

    fix aspect ratio

--- pages/login.vue
+++ pages/login.vue
@@ -2,7 +2,7 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="10" xl="8">
       <template>
-        <v-img src="/wip.svg" aspect-ratio="3,0788"></v-img>
+        <v-img aspect-ratio="3.0788" src="/wip.svg"></v-img>
       </template>
     </v-col>
   </v-row>
