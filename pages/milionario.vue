019f233c3c02bb43e6c47d41ac35c0cb2d68489e
Matteo Colombo <matte.colo@gmail.com>
2021-03-10 22:43:29 +0100

    Add millionaire page

--- pages/milionario.vue
+++ pages/milionario.vue
@@ -1,12 +1,31 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="10" xl="8">
+    <v-col cols="12" sm="10" xl="8" align="center">
       <template>
-        <h1 class="text-center">Chi vuol essere Milionario?</h1>
-        <video width="100%"  controls>
-          <source src="/gerry.mp4" type="video/mp4" autoplay/>
-          Your browser does not support the video tag.
-        </video>
+        <h1 class="text-center">{{ $t("millionaire.title") }}</h1>
+        <v-img src="/mill.jpg" width="800"></v-img>
+
+        <div class="text-justify" style="max-width:800px;">
+          <h2>{{ $t("millionaire.what") }}</h2>
+          <p
+            v-for="(item, index) in $t('millionaire.whatdesc')"
+            :key="'desc' + index"
+            v-html="item"
+          ></p>
+
+          <h2>{{ $t("millionaire.when") }}</h2>
+          <p v-html="$t('millionaire.whendesc')"></p>
+
+          <h2>{{ $t("millionaire.how") }}</h2>
+          <p
+            v-for="(item, index) in $t('millionaire.howdesc')"
+            :key="'how' + index"
+            v-html="item"
+          ></p>
+
+          <h2>{{ $t("millionaire.prizes") }}</h2>
+          <p v-html="$t('millionaire.prizedesc')"></p>
+        </div>
       </template>
     </v-col>
   </v-row>
@@ -16,35 +35,41 @@
 export default {
   head() {
     return {
-      title: this.$t("compete.title"),
+      title: this.$t("millionaire.title"),
       meta: [
         {
           hid: "description",
           name: "description",
-          content: this.$t("compete.desc"),
+          content: this.$t("millionaire.desc"),
         },
         {
           hid: "twitter:title",
           name: "twitter:title",
-          content: this.$t("compete.title"),
+          content: this.$t("millionaire.title"),
         },
         {
           hid: "twitter:description",
           name: "twitter:description",
-          content: this.$t("compete.desc"),
+          content: this.$t("millionaire.desc"),
         },
         {
           hid: "og:title",
           property: "og:title",
-          content: this.$t("compete.title"),
+          content: this.$t("millionaire.title"),
         },
         {
           hid: "og:description",
           property: "og:description",
-          content: this.$t("compete.desc"),
+          content: this.$t("millionaire.desc"),
         },
       ],
     };
   },
 };
 </script>
+
+<style>
+a {
+  text-decoration: none;
+}
+</style>
